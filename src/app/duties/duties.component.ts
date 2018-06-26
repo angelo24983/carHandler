7d2e1dfa2fa4ec07981aba5e685ce057b36431e4
Angelo Guzzo <angelo.guzzo@agenziademanio.it>
2018-06-26 17:22:26 +0200

    added css to list and chance to select and highlight duties

--- src/app/duties/duties.component.ts
+++ src/app/duties/duties.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Duty } from '../shared/duty';
+import { DUTIES } from '../shared/mock-duties';
 
 @Component({
   selector: 'app-duties',
@@ -9,10 +10,17 @@ import { Duty } from '../shared/duty';
 export class DutiesComponent implements OnInit {
 
   duty = new Duty(1, 'Assicurazione');
+  duties = DUTIES;
+
+  selectedDuty: Duty;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(duty: Duty): void {
+    this.selectedDuty = duty;
   }
 
 }
---
+++ src/app/shared/mock-duties.ts
@@ -0,0 +1,10 @@
+import { Duty } from './duty';
+
+export const DUTIES: Duty[] = [
+  { id: 1, name: 'Assicurazione Rca' },
+  { id: 2, name: 'Bollo' },
+  { id: 3, name: 'Gomme' },
+  { id: 4, name: 'Tagliando' },
+  { id: 5, name: 'Magneta' },
+  { id: 6, name: 'Assicurazione Furto' }
+];
